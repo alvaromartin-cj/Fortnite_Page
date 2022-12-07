e83cde6741baff35f2554bcb8d9533afea518154
alvaromartin-cj <alvaromartincj@gmail.com>
2022-12-08 00:05:35 +0100

    Proyecto creado

---
+++ validacion.js
@@ -0,0 +1,15 @@
+
+
+		confirm("BIENVENIDO A FORTNITE NEWS");
+	
+	
+	const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
+        if(edadDelUsuario >= 18){
+            alert("Eres mayor de edad");
+        }else{
+	    alert("Acceso denegado, eres menor de edad");
+        location.replace('http://www.google.com');
+
+        }
+
+
